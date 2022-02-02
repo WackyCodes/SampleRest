08300b8e0e06e5da2f4c16e3d209657175fd76db
Shailendra Lodhi <wackycodes@gmail.com>
2022-02-02 18:20:59 +0530

    Initial commit

---
+++ spring-boot/src/main/java/com/wackycodes/rest/sample/HelloWorldCounter.java
@@ -0,0 +1,22 @@
+package com.wackycodes.rest.sample;
+
+import com.wackycodes.rest.model.Greeting;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@Controller
+public class HelloWorldCounter {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/helloWorld")
+    @ResponseBody
+    public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="WackyCodes!! Check : https://linktr.ee/wackycodes") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+}
